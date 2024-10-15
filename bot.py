91a535b54627658db52070b087710eb974d4129e
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-15 21:01:04 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.session.aiohttp import AiohttpSession
+import logging
+
+API_TOKEN = '7327562948:AAGiH_qsu4Uh5816u2F26Y6ifOgxPu9QPdc'
+session = AiohttpSession("")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+logging.basicConfig(level=logging.INFO)
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+async def start_bot():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_bot())
