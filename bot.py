3f16f852da50516ffbc32e65f4c5b399fda007a0
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-19 21:21:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import asyncio
-
+import uvicorn
+from fastapi import FastAPI
 from aiogram import Bot, Dispatcher, types
-from aiogram.client.session.aiohttp import AiohttpSession
 import logging
 
 API_TOKEN = '7327562948:AAGiH_qsu4Uh5816u2F26Y6ifOgxPu9QPdc'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
 
 @dp.message()
 async def echo(message: types.Message):
@@ -16,6 +18,14 @@ async def echo(message: types.Message):
 async def start_bot():
     await dp.start_polling(bot)
 
+async def start_server():
+    config = uvicorn.Config(app, host="0.0.0.0", port=8080)
+    server = uvicorn.Server(config)
+    await server.serve()
+
 
 if __name__ == "__main__":
-    asyncio.run(start_bot())
+    loop = asyncio.new_event_loop()
+    loop.create_task(start_server())
+    loop.create_task(start_bot())
+    loop.run_forever()
