4062f8659e255ac45eb90ec342fc404bf75bd8d1
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-19 22:08:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import uvicorn
 from fastapi import FastAPI
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 import logging
 
 API_TOKEN = '7327562948:AAGiH_qsu4Uh5816u2F26Y6ifOgxPu9QPdc'
@@ -11,21 +11,28 @@ logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
-@dp.message()
-async def echo(message: types.Message):
+
+@dp.message(F.text == "/start")
+async def start(message: types.Message):
     await message.answer(message.text)
 
 async def start_bot():
     await dp.start_polling(bot)
 
 async def start_server():
-    config = uvicorn.Config(app, host="0.0.0.0", port=8080)
+    config = uvicorn.Config(app, host="0.0.0.0", port=8090)
     server = uvicorn.Server(config)
     await server.serve()
+
+async def inactive():
+    while True:
+        print("ok")
+        await asyncio.sleep(40)
 
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     loop.create_task(start_server())
     loop.create_task(start_bot())
+    loop.create_task(inactive())
     loop.run_forever()
