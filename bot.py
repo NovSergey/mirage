55e244133fa79be9430cbceec4808258d4797171
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-19 22:19:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 import asyncio
-import uvicorn
-from fastapi import FastAPI
+from threading import Thread
+
+import requests
+from flask import Flask
 from aiogram import Bot, Dispatcher, types, F
 import logging
 
-API_TOKEN = '7327562948:AAGiH_qsu4Uh5816u2F26Y6ifOgxPu9QPdc'
+API_TOKEN = '7327562948:AAE6xyoci6axfKk-bUK4yHHGig_PCPu3FS4'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
 
-app = FastAPI()
+app = Flask(__name__)
 
+@app.get("/")
+def index():
+    return "ok"
 
 @dp.message(F.text == "/start")
 async def start(message: types.Message):
@@ -19,20 +24,18 @@ async def start(message: types.Message):
 async def start_bot():
     await dp.start_polling(bot)
 
-async def start_server():
-    config = uvicorn.Config(app, host="0.0.0.0", port=8090)
-    server = uvicorn.Server(config)
-    await server.serve()
-
 async def inactive():
     while True:
-        print("ok")
         await asyncio.sleep(40)
+        requests.get("http://0.0.0.0:8090")
 
+def start_server():
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
+    t = Thread(target=start_server)
+    t.start()
     loop = asyncio.new_event_loop()
-    loop.create_task(start_server())
     loop.create_task(start_bot())
     loop.create_task(inactive())
     loop.run_forever()
