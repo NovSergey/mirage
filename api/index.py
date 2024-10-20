54844ffb6539479db226a065993c0fb3992404cc
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-20 14:34:47 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
+import asyncio
+import threading
+
 from fastapi import FastAPI
 
 from tgbot.main import start_bot
@@ -8,8 +11,11 @@ app = FastAPI()
 
 @app.get('/start')
 async def start():
-    await start_bot()
-    return ''
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    bot_thread = threading.Thread(target=loop.run_until_complete, args=(start_bot(),), daemon=True)
+    bot_thread.start()
+    return 'started'
 
 @app.get("/")
 def index():
