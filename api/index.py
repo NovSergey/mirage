4a8b7a019ba86db3985f3c8a83933265b9f1cc76
NovSergey <92638803+NovSergey@users.noreply.github.com>
2024-10-20 14:12:30 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,18 @@
+import asyncio
+from threading import Thread
+
 from fastapi import FastAPI
 
-from tgbot.main import bot, dp
+from tgbot.main import start_bot
 
 
 app = FastAPI()
 
+
 @app.get('/start')
 async def start():
-    await dp.start_polling(bot)
+    t = Thread(target=asyncio.run, args=(start_bot(),))
+    t.start()
     return ''
 
 @app.get("/")
